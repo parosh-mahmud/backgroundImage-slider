f341a9d668286e8ace0c06285d11ddea848f284f
Parosh Mahmud <prparosh@gmail.com>
2020-12-27 08:28:44 +0600

    added all files to repo for the first tme

---
+++ js/app.js
@@ -0,0 +1,79 @@
+// immediate invoked function expression
+
+// (function() {
+//   const pictures = [
+//     "contBcg-0",
+//     "contBcg-1",
+//     "contBcg-2",
+//     "contBcg-3",
+//     "contBcg-4"
+//   ];
+
+//   //select both left and right button. Add event listeners
+//   const buttons = document.querySelectorAll('.btn')
+//   const imageDIV = document.querySelector('.img-container')
+//   let counter = 0
+
+//   buttons.forEach(function(button){
+//     button.addEventListener('click', function(e){
+//       if (button.classList.contains('btn-left')){
+//         counter--
+//         if(counter < 0){
+//           counter = pictures.length -1
+//         }
+//         imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+//       }
+//       if (button.classList.contains('btn-right')){
+//         counter++
+//         if(counter > pictures.length -1){
+//           counter = 0
+//         }
+//         imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
+//       }
+//     })
+//   })
+// })();
+
+
+
+
+(function() {
+  const pictures = [
+    "contBcg-0",
+    "contBcg-1",
+    "contBcg-2",
+    "contBcg-3",
+    "contBcg-4"
+  ]
+
+  let buttons = document.querySelectorAll(".btn")
+  let imgDiv = document.querySelector(".img-container")
+
+  let counter = 0
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      e.preventDefault()
+      if (button.classList.contains("btn-left")) {
+        counter--
+        if (counter < 0) {
+          counter = pictures.length-1
+          
+        }
+
+        imgDiv.style.backgroundImage =`url('./img/${pictures[counter]}.jpeg')`
+      }
+
+      if (button.classList.contains("btn-right")) {
+        counter++
+        if (counter>pictures.length-1) {
+          counter = 0
+          console.log(counter)
+        }
+        imgDiv.style.backgroundImage =`url('./img/${pictures[counter]}.jpeg')`
+      }
+
+    })
+  })
+
+
+})()
